83aff091123a07d7b2a94e63f3a4e0752fe73412
Sailab <sailabbanik24@gmail.com>
2023-03-21 19:32:39 +0530

    Discover

--- app/src/main/java/com/sailab/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/sailab/bluetoothapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,15 +27,16 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     ImageView bluetooth_img;
-    TextView my_device_name;
-    Button on_btn, off_btn, devices_btn;
-    ListView lv_paired_devices;
-    BluetoothManager bluetoothManager;
+    TextView my_device_name, devices_tv;
+    Button on_btn, off_btn, scan_discover_btn;
+    ListView lv_paired_devices, lv_scanned_devices;
     BluetoothAdapter bluetoothAdapter;
     Set<BluetoothDevice> bluetoothDevices;
-    ArrayList<String> list = new ArrayList<>();
+    ArrayList<String> scanList;
+    ArrayAdapter<String> arrayAdapter, scanAdapter;
 
     private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVERABLE = 1;
 
     @SuppressLint("MissingPermission")
     @Override
@@ -44,23 +48,39 @@ public class MainActivity extends AppCompatActivity {
         my_device_name = findViewById(R.id.my_device_name);
         on_btn = findViewById(R.id.on_btn);
         off_btn = findViewById(R.id.off_btn);
-        devices_btn = findViewById(R.id.devices_btn);
+        devices_tv = findViewById(R.id.devices_tv);
+        scan_discover_btn = findViewById(R.id.scan_discover_btn);
         lv_paired_devices = findViewById(R.id.lv_paired_devices);
+        lv_scanned_devices = findViewById(R.id.lv_scanned_devices);
 
-        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        scanList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        scanAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_2, scanList);
+        lv_scanned_devices.setAdapter(scanAdapter);
 
         if (bluetoothAdapter == null) {
             Toast.makeText(this, "Device doesn't support Bluetooth", Toast.LENGTH_SHORT).show();
         }
 
+        // Bluetooth Avatar
         if (bluetoothAdapter.isEnabled())
             bluetooth_img.setImageResource(R.drawable.ic_baseline_bluetooth_connected_24);
         else
             bluetooth_img.setImageResource(R.drawable.ic_baseline_bluetooth_disabled_24);
 
+        // Device Name
         my_device_name.setText(bluetoothAdapter.getName());
 
+        // Paired Devices
+        bluetoothDevices = bluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice device : bluetoothDevices)
+            arrayAdapter.add(device.getName());
+        lv_paired_devices.setAdapter(arrayAdapter);
+
+
+        // On Button
         on_btn.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("MissingPermission")
             @Override
@@ -69,11 +89,14 @@ public class MainActivity extends AppCompatActivity {
                     Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
                     bluetooth_img.setImageResource(R.drawable.ic_baseline_bluetooth_connected_24);
-                } else
+                } else {
                     Toast.makeText(MainActivity.this, "Bluetooth Already On", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
+        // Off Button
         off_btn.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("MissingPermission")
             @Override
@@ -84,25 +107,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        devices_btn.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
+        // Pair New Devices Button
+        scan_discover_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(bluetoothAdapter.isEnabled()) {
-                    bluetoothDevices = bluetoothAdapter.getBondedDevices();
-
-                    for (BluetoothDevice bt : bluetoothDevices)
-                        list.add(bt.getName());
-
-                    ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
-                    lv_paired_devices.setAdapter(adapter);
-
-                    lv_paired_devices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            Toast.makeText(MainActivity.this, "Ready to transfer data", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                if (bluetoothAdapter.isEnabled()) {
+                    // Discover
+                    Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                    startActivityForResult(discoverableIntent, REQUEST_DISCOVERABLE);
+                    Toast.makeText(MainActivity.this, "Your device is discoverable as " + bluetoothAdapter.getName(), Toast.LENGTH_LONG).show();
                 } else
                     Toast.makeText(MainActivity.this, "Turn on Bluetooth", Toast.LENGTH_SHORT).show();
             }
